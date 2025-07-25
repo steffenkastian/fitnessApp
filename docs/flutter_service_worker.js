26ebc44dd3d1fc59fd1b98b8a8e546785eb66433
steffenkastian <steffenkastian@googlemail.com>
2025-07-25 20:45:31 +0200

    Jetzt ist es save?

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1514eb11726897c1a7d29a6a71abdd6f
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ce639ab6e714f455f22fdd513c3232b0",
+"flutter_bootstrap.js": "f6353d866324e5f9dad1a7444983779c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
