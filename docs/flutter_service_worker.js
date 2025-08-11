b90a1f2ef8ba94b9d5169549e01c67ffbf2cac1d
steffenkastian <steffenkastian@googlemail.com>
2025-08-11 20:59:48 +0200

    Gehts jetzt?

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c11c39176090bb98a9fd2765ac12082b
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "19b1e135ba516d3035363a0b7a0f0f8b",
+"flutter_bootstrap.js": "77f94e567a123c424b7321ffed4817be",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
